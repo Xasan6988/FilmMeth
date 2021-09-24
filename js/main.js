6ff727f77bfe54a1109e9b49589e91af80d44bab
Atryom <238136@mail.ru>
2021-09-24 15:28:44 +0300

    finish project

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 const headerForm = document.querySelector('.header__search-form');
 const headerInput = document.querySelector('.header__search-input');
+const navigation = document.querySelector('.js-navigation');
+const burgerBtn = document.querySelector('.js-burger-btn');
+const closeBtn = document.querySelector('.js-navigation-close')
 
+
+// стили для строки поиска
 headerInput.addEventListener('focus', () => {
   headerForm.style.boxShadow = '0 0 3px 2px var(--color-primary)';
 });
 
 headerInput.addEventListener('blur', () => {
   headerForm.style.boxShadow = 'none';
-})
+});
+
+
+// стили для бургер-меню
+burgerBtn.addEventListener('click', () => {
+  navigation.style.right = '0';
+});
+
+closeBtn.addEventListener('click', () => {
+  navigation.style.right = '';
+});
